9b72b300935f7fa690cb38dfb90f5488337b2434
Egor Morozov <e.d.morozov@esoft.local>
2025-03-03 21:00:02 +0500

    fix

--- src/components/Sortable/Element.js
+++ src/components/Sortable/Element.js
@@ -4,8 +4,7 @@ import {SortableElement} from "react-sortable-hoc";
 const Element = ({url}) => <img
     style={{
       margin: 20,
-      width: 200,
-      height: 200,
+      maxWidth: 250,
       display: 'flex',
       borderRadius: 10,
       boxShadow: '5px 5px 10px rgba(0, 0, 0, .5)'
